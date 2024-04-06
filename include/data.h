c3e1863a60a38b08ed91c8aec160b5cf8f882f70
Aviral Singh <aviralsingh@protonmail.com>
2024-04-06 15:39:09 +0530

    Add cache parameters

--- include/data.h
+++ include/data.h
@@ -1,11 +1,14 @@
 #pragma once
+#define CACHE_DELAY 1
+#define MEM_DELAY 100
+#define NUM_TRANSFER_BYTES 4
 enum Instruction
 {
     Load,
     Save
 };
 
-enum EvicitionType
+enum EvictionType
 {
     LRU,
     FIFO
@@ -18,16 +21,27 @@ enum WriteType
 
 struct CacheParameters
 {
-    int sets;
-    int blocks;
-    int bytes_per_block;
+    unsigned long sets;
+    unsigned long blocks;
+    unsigned long bytes_per_block;
     bool allocate_on_write;
     WriteType writeType;
-    EvicitionType evict;
+    EvictionType evict;
 };
 
 struct MemReq
 {
     Instruction ins;
     int address;
+};
+
+struct CacheStatistics
+{
+    long long loads = 0;
+    long long load_hits = 0;
+    long long load_misses = 0;
+    long long stores = 0;
+    long long store_hits = 0;
+    long long store_misses = 0;
+    long long cycles = 0;
 };

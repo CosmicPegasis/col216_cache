2c0030f1eebca4bfb2a9435107e6fa3068ad11a4
Aviral Singh <aviralsingh@protonmail.com>
2024-04-06 15:39:31 +0530

    Add a statistics io

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include "arg_parser.h"
+#include "cache.h"
 #include "data.h"
 #include "parser.h"
 #include <iostream>
 
+void print_cache_statistics(const CacheStatistics &stats)
+{
+    std::cout << "Cache Statistics:" << std::endl;
+    std::cout << "  Loads:           " << stats.loads << std::endl;
+    std::cout << "  Load Hits:        " << stats.load_hits << std::endl;
+    std::cout << "  Load Misses:      " << stats.load_misses << std::endl;
+    std::cout << "  Stores:          " << stats.stores << std::endl;
+    std::cout << "  Store Hits:       " << stats.store_hits << std::endl;
+    std::cout << "  Store Misses:     " << stats.store_misses << std::endl;
+    std::cout << "  Cycles:           " << stats.cycles << std::endl;
+}
 int main(int argc, char **argv)
 {
     try
@@ -22,6 +34,13 @@ int main(int argc, char **argv)
             std::cout << "Instruction: " << (req.ins == Load ? "Load" : "Save") << ", Address: " << req.address
                       << std::endl;
         }
+
+        Cache cache{params};
+        for (MemReq req : requests)
+        {
+            cache.proc_request(req);
+        }
+        print_cache_statistics(cache.get_statistics());
     }
     catch (const std::exception &e)
     {

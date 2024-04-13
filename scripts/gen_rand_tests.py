627a8ace37e64dbc88c02dd25edacf0d4bc76a2e
Aviral Singh <aviralsingh@protonmail.com>
2024-04-13 13:53:59 +0530

    Add argument for number of tests

--- scripts/gen_rand_tests.py
+++ scripts/gen_rand_tests.py
@@ -3,13 +3,13 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: num_tests output_file_name")
+    if len(sys.argv) != 4:
+        print("Usage: num_tests num_addr output_file_name")
         exit(1)
 
-    file = open(sys.argv[2], "w")
-    for i in range(0, int(sys.argv[1])):
-        addr = random.randint(0, 200)
+    file = open(sys.argv[3], "w")
+    for i in range(0, int(sys.argv[2])):
+        addr = random.randint(0, int(sys.argv[1]))
         itype = 's' if random.randint(0, 1) == 0 else 'l'
         to_write = f'{itype} {hex(addr)} 0\n'
         # print(to_write)  
